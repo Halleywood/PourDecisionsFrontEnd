0f3941cfff7159d13ac0c56d036b2faa13769203
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-31 19:18:42 -0700

    security module with JWT service and login/reg components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { RegistrationComponent } from './security/registration/registration.component';
+import { LoginComponent } from './security/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
