f9f5aae5fddc1fde3cf50c4477488c175c150cb3
Kelsey Halley <kelseyghalley@gmail.com>
2023-05-31 19:23:53 -0700

    declerations/imports for security module

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppRoutingModule } from '../app-routing';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, RegistrationComponent],
   imports: [
-    CommonModule
+    CommonModule, 
+    AppRoutingModule
+  ], 
+  exports: [
+    LoginComponent, 
+    RegistrationComponent
   ]
 })
 export class SecurityModule { }
