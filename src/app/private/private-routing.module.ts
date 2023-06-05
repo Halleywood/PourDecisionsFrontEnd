03924568f3851fea493e2fd04313c8ad23f6d632
Kelsey Halley <kelseyghalley@gmail.com>
2023-06-05 13:29:41 -0700

    setting up community pages

--- src/app/private/private-routing.module.ts
+++ src/app/private/private-routing.module.ts
@@ -4,10 +4,13 @@ import { RouterModule, Routes } from '@angular/router'
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { AuthGuard } from '../services/auth.guard';
 import { WineDetailsComponent } from './components/wine-details/wine-details.component';
+import { WinesComponent } from './components/wines/wines.component';
 
 const routes: Routes = [
-    {path: 'home', component: HomepageComponent, canActivate: [AuthGuard]}, 
-    {path: 'wine/:id', component: WineDetailsComponent, canActivate: [AuthGuard]},
+    {path: 'home', component: HomepageComponent, canActivate: [AuthGuard]},
+    {path: 'wines', component: WinesComponent, canActivate: [AuthGuard]},
+    {path: 'community', component:HomepageComponent, canActivate: [AuthGuard]},
+    {path: 'wines/:id', component: WineDetailsComponent, canActivate: [AuthGuard]},
     {path: '**', redirectTo: 'home', pathMatch: "full"}
 ]
 
