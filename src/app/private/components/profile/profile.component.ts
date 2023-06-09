57fc0483b5d8ffc93feea6e620cceb88f81b5b3a
Kelsey Halley <kelseyghalley@gmail.com>
2023-06-08 21:55:05 -0700

    done. App is mobile response, cleaned up any fake hardcoded links

--- src/app/private/components/profile/profile.component.ts
+++ src/app/private/components/profile/profile.component.ts
@@ -4,6 +4,7 @@ import {Observable } from 'rxjs'
 import { UserProfile } from 'src/app/userprofile.model';
 import { WineService } from 'src/app/services/wine.service';
 import { Post } from 'src/app/post.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -13,26 +14,22 @@ import { Post } from 'src/app/post.model';
 export class ProfileComponent implements OnInit{
 
 
-  userPosts: Post[] = [];
+
    currentUser$: Observable<UserProfile | null> = new Observable<UserProfile | null>()
 
-    constructor(private userService: UserService, private wineService: WineService){
+    constructor(private userService: UserService, private wineService: WineService, private router: Router){
 
     }
     //on init, gets the userprofile from logged in user and their posts. 
     ngOnInit(){
       this.currentUser$ =this.userService.getCurrentUser();
-      this.getAllPosts();
+   
     }
 
-    //Retrieves all posts written by the user. 
-    public getAllPosts(){
-        return this.wineService.getAllUsersPosts().subscribe((userPosts: Post[])=>{
-          this.userPosts = userPosts;
-          console.log(this.userPosts)
-        }, (error) =>{
-          console.log("Error fetching posts:", error)
-        });
+
+    public logOutUser(){
+      this.userService.logout();
+      this.router.navigate(['/public/login'])
     }
 
 }
